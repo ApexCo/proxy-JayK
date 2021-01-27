2545923d9b3c06e131527696f1a4f28f0c00e703
Steve Kriz <steve.kriz@gmail.com>
2021-01-26 23:42:23 -0800

    Implement proxy server.

---
+++ server/api.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const axios = require('axios');
+
+const api = express.Router();
+
+api.get('/galleries/:id', (req, res) => {
+  axios.get(`http://72.134.126.46:8080/api/galleries/${req.params.id}`)
+    .then(({data}) => res.status(200).send(data))
+    .catch((err) => res.status(400).send(err));
+});
+
+api.get('/checkout/:id', (req, res) => {
+  axios.get(`http://34.219.249.230:8080/api/checkout/${req.params.id}`)
+    .then(({data}) => res.status(200).send(data))
+    .catch((err) => res.status(400).send(err));
+});
+
+api.get('/reviews/:id', (req, res) => {
+  axios.get(`http://localhost:8080/api/reviews/${req.params.id}`)
+    .then(({data}) => res.status(200).send(data))
+    .catch((err) => res.status(400).send(err));
+});
+
+api.get('/img_carousel/:id', (req, res) => {
+  axios.get(`http://3.101.61.202:8080/api/img_carousel/${req.params.id}`)
+    .then(({data}) => res.status(200).send(data))
+    .catch((err) => res.status(400).send(err));
+});
+
+module.exports = api;
---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// const bundles = require('./bundles');
+const api = require('./api');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.get('/', (req, res) => res.redirect('/1'));
+app.use('/:id', express.static(path.join(__dirname, '../public')));
+// app.use('/:id/bundles', bundles);
+app.use('/api', api);
+
+module.exports = app;
