2c3fec86a1e4ea9e91e2118eb342cf7d2bd0586a
Steve Kriz <steve.kriz@gmail.com>
2021-01-31 18:49:03 -0800

    Update proxy.

--- server/api.js
+++ server/api.js
@@ -1,28 +1,28 @@
 const express = require('express');
-const axios = require('axios');
+const { get } = require('axios');
 
 const api = express.Router();
 
 api.get('/galleries/:id', (req, res) => {
-  axios.get(`http://13.52.246.93:8080/api/galleries/${req.params.id}`)
+  get(`http://13.52.246.93:8080/api/galleries/${req.params.id}`)
     .then(({data}) => res.status(200).send(data))
     .catch((err) => res.status(400).send(err));
 });
 
 api.get('/checkout/:id', (req, res) => {
-  axios.get(`http://34.219.249.230:8080/api/checkout/${req.params.id}`)
+  get(`http://34.219.249.230:8080/api/checkout/${req.params.id}`)
     .then(({data}) => res.status(200).send(data))
     .catch((err) => res.status(400).send(err));
 });
 
 api.get('/reviews/:id', (req, res) => {
-  axios.get(`http://54.215.215.126:8080/api/reviews/${req.params.id}`)
+  get(`http://54.215.215.126:8080/api/reviews/${req.params.id}`)
     .then(({data}) => res.status(200).send(data))
     .catch((err) => res.status(400).send(err));
 });
 
 api.get('/img_carousel/:id', (req, res) => {
-  axios.get(`http://13.56.115.246:8080/api/img_carousel/${req.params.id}`)
+  get(`http://13.56.115.246:8080/api/img_carousel/${req.params.id}`)
     .then(({data}) => res.status(200).send(data))
     .catch((err) => res.status(400).send(err));
 });
